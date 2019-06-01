47628d7b1e5fee1a2245b0a9994311c639bcf5bf
David Roundy <roundyd@physics.oregonstate.edu>
2019-06-01 15:38:48 -0700

    add reading and saving things

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ use warp::{Filter, path};
 use display_as::{HTML, display};
 use serde_derive::{Deserialize, Serialize};
 
+mod atomicfile;
+
 fn main() {
     let style_css = path!("style.css").and(warp::fs::file("style.css"));
     let style_css_2 = path!("style.css").and(warp::fs::file("style.css"));
+
+    let things = Thing::read();
+    things.save();
 
     warp::serve(style_css
                 .or(style_css_2))
@@ -23,6 +28,25 @@ struct Thing {
 impl Thing {
     fn priority(&self) -> u64 {
         self.times_used + self.times_skipped
+    }
+    fn read() -> Self {
+        if let Ok(f) = ::std::fs::File::open("things.yaml") {
+            if let Ok(s) = serde_yaml::from_reader::<_,Self>(&f) {
+                return s;
+            }
+        }
+        Thing {
+            name: String::from("Everything"),
+            description: String::from(""),
+            times_used: 0,
+            times_skipped: 0,
+            children: Vec::new(),
+        }
+    }
+    fn save(&self) {
+        let f = atomicfile::AtomicFile::create("things.yaml")
+            .expect("error creating save file");
+        serde_yaml::to_writer(&f, self).expect("error writing yaml")
     }
 }
 
