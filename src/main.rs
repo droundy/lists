4d58e2715166842888497a331be2069b2e0a4290
David Roundy <roundyd@physics.oregonstate.edu>
2019-06-01 06:21:38 -0700

    try defining Thing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use warp::{Filter, path};
 use display_as::{HTML, display};
+use serde_derive::{Deserialize, Serialize};
 
 fn main() {
     let style_css = path!("style.css").and(warp::fs::file("style.css"));
@@ -8,4 +9,31 @@ fn main() {
     warp::serve(style_css
                 .or(style_css_2))
         .run(([0, 0, 0, 0], 3000));
+}
+
+#[derive(Debug, Hash, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct Thing {
+    name: String,
+    description: String,
+    times_used: u64,
+    times_skipped: u64,
+    children: Vec<Thing>,
+}
+
+impl Thing {
+    fn priority(&self) -> u64 {
+        self.times_used + self.times_skipped
+    }
+}
+
+impl PartialOrd for Thing {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Thing {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.priority().cmp(&other.priority())
+    }
 }
